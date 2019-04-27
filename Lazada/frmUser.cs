ebb28829218a055a9ccd0eef478b1ffbb33c196f
unknown <blv.tu97@gmail.com>
2019-04-27 22:10:38 +0700

    Lazada project

---
+++ Lazada/frmUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lazada
+{
+    public partial class frmUser : Form
+    {
+        public frmUser()
+        {
+            InitializeComponent();
+        }
+
+        bool DrawerOpen = true;
+
+        private void BtnToggleDrawer_Click(object sender, EventArgs e)
+        {
+            DrawerOpen = !DrawerOpen;
+            pnlDrawer.Visible = false;
+
+            if (DrawerOpen)
+            {
+                //animated Drawer Open
+                pnlDrawer.Width = 233;
+                btnToggleDrawer.Image = imageList1.Images[0];
+                bunifuTransition1.ShowSync(pnlDrawer);
+            }
+            else
+            {
+                //Aminated Drawer close
+                pnlDrawer.Width = 56;
+                btnToggleDrawer.Image = imageList1.Images[1];
+                bunifuTransition1.ShowSync(pnlDrawer);
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
